535c95b3cddc2b6e3c486c74754c9b1d158c23a8
Wenjie Chen <monroechen1@gmail.com>
2024-01-05 16:30:29 +0800

    Initial layout

---
+++ kse-app/src/pages/MainPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ToolBar from "../components/ToolBar";
+import NavBar from "../components/NavBar";
+import SiteMap from "../components/SiteMap";
+import "../css/Layout.css"
+
+const MainPage = () => {
+    return (
+        <div className="layout">
+            <div className="toolbar">
+                <ToolBar />
+            </div>
+            <div className="navbar">
+                <NavBar />
+            </div>
+            <div className="content">
+                <p>Main</p>
+            </div>
+            <div className="sitemap">
+                <SiteMap />
+            </div>
+        </div>
+    )
+}
+
+export default MainPage;
