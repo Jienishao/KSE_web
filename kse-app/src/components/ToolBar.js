a69b4a813c8e2cdf6eba4e2adba962482f868067
S3622097 <53887162+S3622097@users.noreply.github.com>
2024-01-06 17:39:26 +0800

    version 1.0.0 of logo

--- kse-app/src/components/ToolBar.js
+++ kse-app/src/components/ToolBar.js
@@ -1,8 +1,15 @@
 import React from "react";
+import logoX from "../image/X_logo.jpg"
+import logoFB from "../image/facebook_logo.png"
+import logoW from "../image/wechat_logo.png"
 
 const ToolBar = () => {
     return (
-        <div>ToolBar</div>
+        <div className="media_icon">
+            <img src={logoX} alt="X logo" className="logo"></img>   
+            <img src={logoFB} alt="Face book logo" className="logo"></img>
+            <img src={logoW} alt="wechat logo" className="logo"></img>
+        </div>
     )
 }
 
