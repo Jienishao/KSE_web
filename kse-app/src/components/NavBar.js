a386913d32a40168b288b440929f1c4138b8add3
S3622097 <53887162+S3622097@users.noreply.github.com>
2024-01-17 17:04:21 +0800

    Toolbar version 1.0.1

--- kse-app/src/components/NavBar.js
+++ kse-app/src/components/NavBar.js
@@ -3,9 +3,23 @@ import { PiWindowsLogoBold } from "react-icons/pi";
 
 const NavBar = () => {
     return (
-        <div className="brand_logo">
-            <a className="logo2"> <PiWindowsLogoBold className="react_icon"/></a>
-        </div>
+        <>
+            <div>
+
+            </div>
+            <div className="brand_logo">
+                <a className="logo2"> <PiWindowsLogoBold className="react_icon" /></a>
+            </div>
+            <div className="function">
+                123
+            </div>
+            <div>
+                321
+            </div>
+            <div>
+                1234567
+            </div>
+        </>
     )
 }
 
