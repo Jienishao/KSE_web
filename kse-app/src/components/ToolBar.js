70b5590a5adbcc60f32941742ed32e9c5f39be18
S3622097 <53887162+S3622097@users.noreply.github.com>
2024-01-25 18:40:02 +0800

    Toolbar version 1.0.2 add contact

--- kse-app/src/components/ToolBar.js
+++ kse-app/src/components/ToolBar.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
 import { IoLogoWechat } from "react-icons/io5";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 
 
@@ -9,6 +10,8 @@ const ToolBar = () => {
 
   const [isPopupOpen, setPopupOpen] = useState(false);
   const [buttonClicked, setButtonClicked] = useState(false);
+  const [isContactOpen, setContactOpen] = useState(false);
+  const [contactClicked, setContactClicked] = useState(false);
   const popupRef = useRef(null);
 
   const openPopup = (event) => {
@@ -26,6 +29,17 @@ const ToolBar = () => {
     if (popupRef.current && !popupRef.current.contains(event.target)) {
       closePopup();
     }
+  };
+
+  const openContact = (event) => {
+    event.stopPropagation();
+    setContactOpen(true);
+    setContactClicked(true);
+  };
+
+  const closeContact = () => {
+    setContactOpen(false);
+    setContactClicked(false);
   };
 
   useEffect(() => {
@@ -64,21 +78,32 @@ const ToolBar = () => {
 
       <div></div>
 
-      <div className="phone_box" title="Click to copy" onClick={() => handelCopy("+86 13XXXXXXXXX")}>
-        <div className="p1">
-          Phone:
-        </div>
-        <div className="p2">
-          +86 12345678900
-        </div>
-      </div>
-      <div className="email_box" title="Click to copy" onClick={() => handelCopy("XXXXXXXX@gmail.com")}>
-        <div className="p1">
-          Email:
-        </div>
-        <div className="p2">
-          123456789@gmail.com
-        </div>
+      <div className={`contact_button ${buttonClicked ? "clicked" : ""}`}>
+        <button className="contact" onClick={isContactOpen ? closeContact : openContact}> 
+        {contactClicked ? <MdKeyboardArrowRight className="contact_off"/> : <MdKeyboardArrowLeft className="contact_on"/> }
+        </button>
+
+        {isContactOpen && (
+          <div className="contact_box" ref={popupRef}>
+            <span className="close" onClick={closeContact}></span>
+            <div className="phone_box" title="Click to copy" onClick={() => handelCopy("+86 13XXXXXXXXX")}>
+              <div className="p1">
+                Phone:
+              </div>
+              <div className="p2">
+                +86 12345678900
+              </div>
+            </div>
+            <div className="email_box" title="Click to copy" onClick={() => handelCopy("XXXXXXXX@gmail.com")}>
+              <div className="p1">
+                Email:
+              </div>
+              <div className="p2">
+                123456789@gmail.com
+              </div>
+            </div>
+          </div>
+        )}
       </div>
 
       <div className={`language_button ${buttonClicked ? "clicked" : ""}`}>
