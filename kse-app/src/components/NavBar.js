ab70080d8df500501cb1d1431d228e7855e21baf
S3622097 <53887162+S3622097@users.noreply.github.com>
2024-01-26 17:34:54 +0800

    Navbar version 1.0.1 function popup

--- kse-app/src/components/NavBar.js
+++ kse-app/src/components/NavBar.js
@@ -10,14 +10,26 @@ const NavBar = () => {
             <div className="brand_logo">
                 <a className="logo2"> <PiWindowsLogoBold className="react_icon" /></a>
             </div>
-            <div className="function">
-                123
+            <div className="function1">
+                Function 1
+                <div className="fun_pop1">
+                    {/* Add your popup content here */}
+                    This is a popup!
+                </div>
             </div>
-            <div>
-                321
+            <div className="function2">
+                Function 2
+                <div className="fun_pop2">
+                    {/* Add your popup content here */}
+                    This is a popup2!
+                </div>
             </div>
-            <div>
-                1234567
+            <div className="function3">
+                Function 3
+                <div className="fun_pop3">
+                    {/* Add your popup content here */}
+                    This is a popup3!
+                </div>
             </div>
         </>
     )
