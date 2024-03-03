a2e20c92e0359797f64c1dc5a503342659035b46
Wenjie Chen <monroechen1@gmail.com>
2024-03-03 19:18:47 +1100

    SiteMap ver 1.0.0

--- kse-app/src/components/SiteMap.js
+++ kse-app/src/components/SiteMap.js
@@ -3,10 +3,40 @@ import "../css/SiteMap.css"
 
 const SiteMap = () => {
     return (
-        <div>
-
+        <div className="site">
+            <div className="row">
+                <div className="col">
+                    <h4>COMPANY NAME</h4>
+                    <ul className="list">
+                        <li>123456</li>
+                        <li>LOCATION</li>
+                        <li>ADDRESS</li>
+                    </ul>
+                </div>
+                <div className="col">
+                    <h4>STUFF</h4>
+                    <ul className="list">
+                        <li>ONE</li>
+                        <li>TWO</li>
+                        <li>THREE</li>
+                    </ul>
+                </div>
+                <div className="col">
+                    <h4>Another</h4>
+                    <ul className="list">
+                        <li>ONE</li>
+                        <li>TWO</li>
+                        <li>THREE</li>
+                    </ul>
+                </div>
+            </div>
+            <hr />
+            <div className="row">
+                <p className="col-small">
+                    &copy;{new Date().getFullYear()} COMPANY NAME INC | All right reserved | Terms Of Service | Privicy
+                </p>
+            </div>
         </div>
-
     )
 }
 
