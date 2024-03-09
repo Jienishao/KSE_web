414079bed70c471ca6fd6cd0b80b43f6f3c2129d
Wenjie Chen <monroechen1@gmail.com>
2024-03-09 22:26:14 +1100

    Slider ver 1.0.1 & update MainPage

--- kse-app/src/assets/SlideImages.js
+++ kse-app/src/assets/SlideImages.js
@@ -2,22 +2,30 @@ const SlideImages = [
     {
         imgURL:
             "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        imgAlt: "img-1"
+        imgAlt: "img-1",
+        caption: "Caption for Image 1",
+        buttonText: "Button 1"
     },
     {
         imgURL:
             "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        imgAlt: "img-2"
+        imgAlt: "img-2",
+        caption: "Caption for Image 2",
+        buttonText: "Button 2"
     },
     {
         imgURL:
             "https://images.pexels.com/photos/1128678/pexels-photo-1128678.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        imgAlt: "img-3"
+        imgAlt: "img-3",
+        caption: "Caption for Image 3",
+        buttonText: "Button 3"
     },
     {
         imgURL:
             "https://images.pexels.com/photos/54455/cook-food-kitchen-eat-54455.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-        imgAlt: "img-4"
+        imgAlt: "img-4",
+        caption: "Caption for Image 4",
+        buttonText: "Button 4"
     }
 ];
 
--- kse-app/src/pages/MainPage.js
+++ kse-app/src/pages/MainPage.js
@@ -17,14 +17,37 @@ const MainPage = () => {
             </div>
             <div className="content">
                 <Slider>
-                    {SlideImages.map((SlideImages, index) => {
-                        return <img key={index} src={SlideImages.imgURL} alt={SlideImages.imgAlt} />;
-                    })}
+                    {SlideImages.map((slide, index) => (
+                        <div key={index} className="slide">
+                            <img src={slide.imgURL} alt={slide.imgAlt} />
+                            <div className="caption">{slide.caption}</div>
+                            <button>{slide.buttonText}</button>
+                        </div>
+                    ))}
                 </Slider>
+                <div className="title_1">
+                    <h2>Additional Content</h2>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <img src="your-image-url-1" alt="Image 1" />
+                        <div>Additional Content 1</div>
+                    </div>
+                    <div className="card">
+                        <img src="your-image-url-1" alt="Image 1" />
+                        <div>Additional Content 2</div>
+                    </div>
+                    <div className="card">
+                        <img src="your-image-url-1" alt="Image 1" />
+                        <div>Additional Content 3</div>
+                    </div>
+                </div>
+
             </div>
-            <div className="sitemap">
+
+            {/*<div className="sitemap">
                 <SiteMap />
-            </div>
+            </div>*/}
         </div>
     )
 }
