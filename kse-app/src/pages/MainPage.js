1d8e58efe952b060a866e49f9121c31ab0a6888e
Wenjie Chen <monroechen1@gmail.com>
2024-03-14 14:21:08 +1100

    little update

--- kse-app/src/pages/MainPage.js
+++ kse-app/src/pages/MainPage.js
@@ -25,7 +25,83 @@ const MainPage = () => {
                         </div>
                     ))}
                 </Slider>
-                <div className="title_1">
+
+                <div class="two_sides">
+                    <div class="left_side">
+                        <div class="title_1">
+                            <h2>Additional Content</h2>
+                        </div>
+                        <button>button</button>
+                    </div>
+                    <div class="right_side">
+                        <img src="path/to/your/image.jpg" alt="Image" />
+                    </div>
+                </div>
+
+                <div class="two_sides">
+                    <div class="left_side">
+                        <img src="path/to/your/image.jpg" alt="Image" />
+                    </div>
+                    <div class="right_side">
+                        <div class="title_1">
+                            <h2>Additional Content</h2>
+                        </div>
+                        <button>button</button>
+
+                    </div>
+                </div>
+
+                <div class="two_sides">
+                    <div class="left_side">
+                        <div class="title_1">
+                            <h2>Additional Content</h2>
+                        </div>
+                        <button>button</button>
+                    </div>
+                    <div class="right_side">
+                        <img src="path/to/your/image.jpg" alt="Image" />
+                    </div>
+                </div>
+
+
+
+                <div className="title_2">
+                    <h2>Additional Content</h2>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <img src="your-image-url-1" alt="Image 1" />
+                        <div>Additional Content 1</div>
+                    </div>
+                    <div className="card">
+                        <img src="your-image-url-1" alt="Image 1" />
+                        <div>Additional Content 2</div>
+                    </div>
+                    <div className="card">
+                        <img src="your-image-url-1" alt="Image 1" />
+                        <div>Additional Content 3</div>
+                    </div>
+                </div>
+
+                <div className="title_2">
+                    <h2>Additional Content</h2>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <img src="your-image-url-1" alt="Image 1" />
+                        <div>Additional Content 1</div>
+                    </div>
+                    <div className="card">
+                        <img src="your-image-url-1" alt="Image 1" />
+                        <div>Additional Content 2</div>
+                    </div>
+                    <div className="card">
+                        <img src="your-image-url-1" alt="Image 1" />
+                        <div>Additional Content 3</div>
+                    </div>
+                </div>
+
+                <div className="title_2">
                     <h2>Additional Content</h2>
                 </div>
                 <div className="cards">
@@ -47,8 +123,8 @@ const MainPage = () => {
 
             {/*<div className="sitemap">
                 <SiteMap />
-            </div>*/}
-        </div>
+        </div>*/}
+        </div >
     )
 }
 
